5734d15c1bb72ca45281e5f68abd70e8176686d3
Raghu K <raghuk.vit@gmail.com>
2025-03-14 06:31:53 +0530

    RoboSHop Project

--- db/run.sh
+++ db/run.sh
@@ -1,3 +1,5 @@
+echo 'show databases' | mysql -h$DB_HOST -u$DB_USER -p$DB_PASS | grep cities
+if [ $? -eq 0 ]; then exit 0 ; fi
 for i in  schema app-user master-data; do
   mysql -h$DB_HOST -u$DB_USER -p$DB_PASS < $i.sql
 done
